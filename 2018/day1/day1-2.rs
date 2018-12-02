bc87a4c1f9ea543ed23c84e4eb850ba7ba0c407b
Svip <svip@diku.dk>
2018-12-02 09:28:14 +0100

    Simplified day1-2 further using .cycle().

--- 2018/day1/day1-2.rs
+++ 2018/day1/day1-2.rs
@@ -10,21 +10,13 @@ fn main() -> io::Result<()> {
 	
 	let mut f = 0;
 	let mut set: HashSet<i64> = HashSet::new();
-	let mut lines = buffer.lines();
-	loop {
-		match lines.next() {
-			Some(line) => {
-				f += line.to_string().parse::<i64>().unwrap();
-				if set.contains(&f) {
-					println!("{}", f);
-					break;
-				}
-				set.insert(f);
-			}
-			None => {
-				lines = buffer.lines();
-			}
+	for l in buffer.lines().cycle() {
+		f += l.to_string().parse::<i64>().unwrap();
+		if set.contains(&f) {
+			println!("{}", f);
+			break;
 		}
+		set.insert(f);
 	}
 	
 	Ok(())
