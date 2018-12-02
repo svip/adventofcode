d3fdd9728f65c1f8c797a1f51e69b890cb03a3a3
Svip <svip@diku.dk>
2018-12-02 13:43:19 +0100

    Modified day 2 part 1 by combining both versions.

--- 2018/day02/day2-1.rs
+++ 2018/day02/day2-1.rs
@@ -1,31 +1,21 @@
 use std::io;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
 	let stdin = io::stdin();
-	let count = stdin.lock().lines()
-		.map(|l| {
-			let mut chars = l.unwrap().chars().collect::<Vec<char>>(); 
-			chars.sort_by(|x, y| x.cmp(y));
-			// First we combine our strings as we see repeats.
-			chars.iter().scan(" ".to_string(), |s, &x| {
-				if (*s).find(x) != None {
-					(*s).push(x);
-				} else {
-					*s = x.to_string();
-				}
-				
-				Some((*s).clone())
-			// If there are three (e.g. "aaa"), there will also be an element
-			// in the list (appearing just beforehand!) that is two (e.g. "aa"),
-			// therefore we subtract when we see len() == 3.
-			}).fold((0, 0), |a, x| {
-				if x.len() == 3 { (a.0 - 1, a.1 + 1) }
-				else if x.len() == 2 { (a.0 + 1, a.1) }
-				else { (a.0, a.1) }
-			})
+	let mut set: HashMap<char, i32> = HashMap::new();
+	let count = stdin.lock().lines().map(|x| {
+		set.clear();
+		for c in x.unwrap().chars() {
+			*set.entry(c).or_insert(0) += 1;
+		}
+		set.values().fold((false,false), |a, &x| {
+			if x == 2 { (true, a.1) }
+			else if x == 3 { (a.0, true) }
+			else { (a.0, a.0) }
 		})
-		.fold((0, 0), |a, x| (a.0 + (x.0 as i64).signum(), a.1 + (x.1 as i64).signum()));
+	}).fold((0,0), |a, x| (a.0 + (x.0 as i64), a.1 + (x.1 as i64)));
 	println!("{}", count.0*count.1);
 	
 	Ok(())
