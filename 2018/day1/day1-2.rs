694ab19312dd2d1884e204497626dc9bae8a29f6
Svip <svip@diku.dk>
2018-12-02 08:52:44 +0100

    Simplified day1-2, because I thought ::parse() could not handle +.

--- 2018/day1/day1-2.rs
+++ 2018/day1/day1-2.rs
@@ -5,23 +5,18 @@ use std::collections::HashSet;
 fn main() -> io::Result<()> {
 	let mut buffer = String::new();
 	let stdin = io::stdin();
-	let mut handle = stdin.lock();
-	
+	let mut handle = stdin.lock();	
 	handle.read_to_string(&mut buffer)?;
+	
 	let mut f = 0;
 	let mut set: HashSet<i64> = HashSet::new();
 	let mut lines = buffer.lines();
 	loop {
 		match lines.next() {
 			Some(line) => {
-				let tmp = line[1..].to_string().parse::<i64>().unwrap();
-				if line.chars().next().unwrap() == '+' {
-					f += tmp;
-				} else {
-					f -= tmp;
-				}
+				f += line.to_string().parse::<i64>().unwrap();
 				if set.contains(&f) {
-					println!("Found: {} ({} matches)", f, set.len());
+					println!("{}", f);
 					break;
 				}
 				set.insert(f);
