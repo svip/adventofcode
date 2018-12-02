28165327563e7540a05e817e3a656de7d1b9fa77
Svip <svip@diku.dk>
2018-12-02 08:48:23 +0100

    Simplified day1-1 by using map() and sum().

--- 2018/day1/day1-1.rs
+++ 2018/day1/day1-1.rs
@@ -2,18 +2,9 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() -> io::Result<()> {
-	let mut f = 0;
 	let stdin = io::stdin();
-	for l in stdin.lock().lines() {
-		let line = l.unwrap();
-		let tmp = line[1..].to_string().parse::<i64>().unwrap();
-		if line.chars().next().unwrap() == '+' {
-			f += tmp;
-		} else {
-			f -= tmp;
-		}
-	}
-	
-	println!("{}", f);
+	println!("{}", stdin.lock().lines().map(|l|
+		l.unwrap().to_string().parse::<i64>().unwrap()
+	).sum::<i64>());
 	Ok(())
 }
