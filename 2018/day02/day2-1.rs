9903102bfb3b0ff8a86ed9a9271e0845901ba322
Svip <svip@diku.dk>
2018-12-02 13:25:11 +0100

    Rewrote day 2 part 1.

--- 2018/day02/day2-1.rs
+++ 2018/day02/day2-1.rs
@@ -1,25 +1,32 @@
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
 	let stdin = io::stdin();
-	let (mut v2, mut v3) = (0, 0);
-	let mut set: HashMap<char, i32> = HashMap::new();
-	for l in stdin.lock().lines() {
-		set.clear();
-		for c in l.unwrap().chars() {
-			*set.entry(c).or_insert(0) += 1;
-		}
-		
-		if set.values().any(|&v| v == 2) {
-			v2 += 1;
-		}
-		if set.values().any(|&v| v == 3) {
-			v3 += 1;
-		}
-	}
-	println!("{}", v2*v3);
+	let count = stdin.lock().lines()
+		.map(|l| {
+			let mut chars = l.unwrap().chars().collect::<Vec<char>>(); 
+			chars.sort_by(|x, y| x.cmp(y));
+			// First we combine our strings as we see repeats.
+			chars.iter().scan(" ".to_string(), |s, &x| {
+				if (*s).find(x) != None {
+					(*s).push(x);
+				} else {
+					*s = x.to_string();
+				}
+				
+				Some((*s).clone())
+			// If there are three (e.g. "aaa"), there will also be an element
+			// in the list (appearing just beforehand!) that is two (e.g. "aa"),
+			// therefore we subtract when we see len() == 3.
+			}).fold((0, 0), |a, x| {
+				if x.len() == 3 { (a.0 - 1, a.1 + 1) }
+				else if x.len() == 2 { (a.0 + 1, a.1) }
+				else { (a.0, a.1) }
+			})
+		})
+		.fold((0, 0), |a, x| (a.0 + (x.0 as i64).signum(), a.1 + (x.1 as i64).signum()));
+	println!("{}", count.0*count.1);
 	
 	Ok(())
 }
