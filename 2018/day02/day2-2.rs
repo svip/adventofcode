bbd7b66430b563e24c084c65f4c4ab16b4836ae7
Svip <svip@diku.dk>
2018-12-03 17:00:16 +0100

    Vastly improved speed of day 2 part 2 solution.

--- 2018/day02/day2-2.rs
+++ 2018/day02/day2-2.rs
@@ -5,23 +5,31 @@ use std::collections::HashSet;
 fn main() -> io::Result<()> {
 	let stdin = io::stdin();
 	let mut lines: HashSet<String> = HashSet::new();
-	println!("{}", stdin.lock().lines().map(|l| {
+	'outer: for l in stdin.lock().lines() {
 		let line = l.unwrap();
-		// Create a list of previous strings with only the characters they
-		// have in common.  And find the longest of those.
-		let s = lines.iter().fold(String::new(), |mut longest, x| {
-			let s = x.chars().zip(line.chars())
-				.filter_map(|x| if x.0 == x.1 { Some(x.0) } else { None } )
-				.collect::<String>();
-			if s.len() > (*longest).len() { longest.clear(); longest.push_str(s.as_str()); }
-			longest
-		});
-		lines.insert(line);	
-		s
-	}).fold(String::new(), |mut longest, x| {
-		if x.len() > (*longest).len() { longest.clear(); longest.push_str(x.as_str()); }
-		longest
-	}));
+		'inner: for testline in lines.iter() {
+			let mut result = line.clone(); // Our working line.
+			// The characters we are testing against.
+			let mut testchars = testline.chars();
+			// Since the lines are equal in size, we should never be calling
+			// .next() on testchars when it would return None.
+			for (i, c) in line.char_indices() {
+				// Attempt to remove the ones that don't match.
+				if c != testchars.next().unwrap() {
+					// If we are about to make the string too distinct (more than
+					// 1 difference) from the current one, it's not the right one.
+					if result.len() == line.len() - 1 {
+						continue 'inner;
+					}
+					result.remove(i);
+				}
+			}
+			// If we ever get down here, it's over.
+			println!("{}", result);
+			break 'outer;
+		}
+		lines.insert(line);
+	}
 	
 	Ok(())
 }
