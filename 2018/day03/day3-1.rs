fdcb5f6784bbac1dc20da01614cb4c855a0572e1
Svip <svip@diku.dk>
2018-12-03 17:58:06 +0100

    Improved speed of day 3 solutions by using arrays instead of HashMap.

--- 2018/day03/day3-1.rs
+++ 2018/day03/day3-1.rs
@@ -1,36 +1,28 @@
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
 	// We know the full size of the map
 	let worldwidth: i32 = 1000;
-	let worldheight: i32 = 1000;
 	let stdin = io::stdin();
-	let mut map: HashMap<i32, i32> = HashMap::with_capacity((worldwidth*worldheight) as usize);
+	let mut map: [i8; 1000 * 1000] = [0; 1000*1000];
 	for l in stdin.lock().lines() {
 		let line = l.unwrap();
 		let v: Vec<&str> = line.split(|c| c == '@' || c == ',' || c == ':' || c == 'x').collect();
 		let (left, top, width, height) = (
-			v[1].to_string().trim().to_string().parse::<i32>().unwrap(),
-			v[2].to_string().parse::<i32>().unwrap(),
-			v[3].to_string().trim().to_string().parse::<i32>().unwrap(),
-			v[4].to_string().parse::<i32>().unwrap());
+			v[1].trim().parse::<i32>().unwrap(),
+			v[2].parse::<i32>().unwrap(),
+			v[3].trim().parse::<i32>().unwrap(),
+			v[4].parse::<i32>().unwrap());
 		let mut pos = left + top * worldwidth;
 		for _ in 0..height {
 			for x in 0..width {
-				*map.entry(pos + x).or_insert(0) += 1;
+				map[(pos + x) as usize] += 1;
 			}
 			pos += worldwidth;
 		}
 	}
-	let mut c = 0;
-	for v in map.values() {
-		if *v > 1 {
-			c += 1;
-		}
-	}
-	println!("{}", c);
+	println!("{}", map.iter().filter(|x| *x > &1).count());
 	
 	Ok(())
 }
--- 2018/day03/day3-2.rs
+++ 2018/day03/day3-2.rs
@@ -1,31 +1,33 @@
 use std::io;
 use std::io::prelude::*;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 fn main() -> io::Result<()> {
 	// We know the full size of the map
 	let worldwidth: i32 = 1000;
-	let worldheight: i32 = 1000;
 	let stdin = io::stdin();
-	let mut map: HashMap<i32, i32> = HashMap::with_capacity((worldwidth*worldheight) as usize);
+	let mut map: [i32; 1000*1000] = [0; 1000*1000];
 	let mut set: HashSet<i32> = HashSet::new();
 	for l in stdin.lock().lines() {
 		let line = l.unwrap();
 		let v: Vec<&str> = line.split(|c| c == '@' || c == ',' || c == ':' || c == 'x').collect();
 		let (claim, left, top, width, height) = (
-			v[0].to_string().trim_matches('#').trim().to_string().parse::<i32>().unwrap(),
-			v[1].to_string().trim().to_string().parse::<i32>().unwrap(),
-			v[2].to_string().parse::<i32>().unwrap(),
-			v[3].to_string().trim().to_string().parse::<i32>().unwrap(),
-			v[4].to_string().parse::<i32>().unwrap());
+			v[0].trim_matches(|c| c == '#' || c == ' ').parse::<i32>().unwrap(),
+			v[1].trim().parse::<i32>().unwrap(),
+			v[2].parse::<i32>().unwrap(),
+			v[3].trim().parse::<i32>().unwrap(),
+			v[4].parse::<i32>().unwrap());
 		let mut pos = left + top * worldwidth;
 		let mut unique = true;
 		for _ in 0..height {
 			for x in 0..width {
-				let t = map.entry(pos + x).or_insert(claim);
-				if *t != claim {
-					set.remove(&*t);
-					*t = 0;
+				let id: usize = (pos + x) as usize;
+				if map[id] == 0 {
+					map[id] = claim;
+				}
+				if map[id] != claim {
+					set.remove(&map[id]);
+					map[id] = -1;
 					unique = false;
 				}
 			}
@@ -35,9 +37,7 @@ fn main() -> io::Result<()> {
 			set.insert(claim);
 		}
 	}
-	for k in &set {
-		println!("{}", k);
-	}
+	println!("{}", set.iter().next().unwrap());
 	
 	Ok(())
 }
