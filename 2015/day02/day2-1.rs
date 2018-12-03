4681e4cff455895e1bba4580e824a8c34e0046b7
Svip <svip@theinfosphere.org>
2018-12-03 19:05:38 +0100

    Updated 2015 day 2 part 1.

--- 2015/day02/day2-1.rs
+++ 2015/day02/day2-1.rs
@@ -6,10 +6,8 @@ fn main() -> io::Result<()> {
 	let mut totalarea = 0;
 	for l in stdin.lock().lines() {
 		let line = l.unwrap();
-		let v: Vec<&str> = line.split('x').collect();
-		let (l, w, h) = (v[0].parse::<i32>().unwrap(), 
-			v[1].parse::<i32>().unwrap(), 
-			v[2].parse::<i32>().unwrap());
+		let v: Vec<i32> = line.split('x').map(|c| c.parse::<i32>().unwrap()).collect();
+		let (l, w, h) = (v[0], v[1], v[2]);
 		let mut a = vec![l*w, w*h, h*l];
 		a.sort_by(|x, y| x.cmp(y));
 		totalarea += a[0] + a[0] * 2 + a[1] * 2 + a[2] * 2;
